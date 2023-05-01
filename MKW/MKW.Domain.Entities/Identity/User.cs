871715c439a0a688699a750d397c6aac07a321c5
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-05-01 02:39:43 -0300

    feat: criacao usuario identity base

---
+++ MKW/MKW.Domain.Entities/Identity/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKW.Domain.Entities.Identity
+{
+    public class User : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
