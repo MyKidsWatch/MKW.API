ffbc072edb9d4737d6c2671f06d503a009afcd3c
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-05-02 20:05:22 -0300

    feat: configuracao de Mapping Profile para DTOs da entidade User com AutoMapper

---
+++ MKW/MKW.Domain.ViewModel/MapperProfile/Identity/IdentityProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MKW.Domain.Dto.Identity;
+using MKW.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKW.Domain.Dto.MapperProfile.Identity
+{
+    public class IdentityProfile : Profile
+    {
+        public IdentityProfile()
+        {
+            CreateMap<CreateUserDTO, User>();
+            CreateMap<User, ReadUserDTO>();
+            CreateMap<IEnumerable<User>, IEnumerable<ReadUserDTO>>();
+            CreateMap<UpdateUserDTO, User>();
+            CreateMap<DeleteUserDTO, User>();
+        }
+    }
+}
