9829cf85c49afd0a45ec32c531585a7492bfd03b
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-05-04 23:11:55 -0300

    feat: configuracao profile do automapper para TokenDTO

---
+++ MKW/MKW.Domain.ViewModel/MapperProfile/IdentityUserProfile/AuthProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MKW.Domain.Dto.DTO.IdentityDTO.Auth;
+using MKW.Domain.Dto.IdentityDTO;
+using MKW.Domain.Entities.IdentityAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKW.Domain.Dto.MapperProfile.IdentityUserProfile
+{
+    public class AuthProfile : Profile
+    {
+        public AuthProfile()
+        {
+            CreateMap<ApplicationToken, TokenDTO>();
+        }
+    }
+}
