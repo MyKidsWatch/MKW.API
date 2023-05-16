1b770796b3a54ec845526c91cd321dab65df3a55
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2023-05-16 18:20:02 -0300

    feat: criando dados iniciais tabela de genero

--- MKW/MKW.Data.Infra/Mapping/UserAggregate/GenderMap.cs
+++ MKW/MKW.Data.Infra/Mapping/UserAggregate/GenderMap.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MKW.Domain.Entities.IdentityAggregate;
 using MKW.Domain.Entities.UserAggregate;
 
 namespace MKW.Data.Context.Mapping.UserAggregate
@@ -53,6 +55,43 @@ namespace MKW.Data.Context.Mapping.UserAggregate
                 .HasMany(x => x.People)
                 .WithOne(x => x.Gender)
                 .HasForeignKey(x => x.GenderId);
+
+            Gender masc = new()
+            {
+                Id = 1,
+                Name = "Masculino",
+                IsBinary = true,
+                UUID = Guid.NewGuid(),
+                CreateDate = DateTime.Now,
+                AlterDate = DateTime.Now,
+                Active = true,
+            };
+
+            Gender fem = new()
+            {
+                Id = 2,
+                Name = "Feminino",
+                IsBinary = true,
+                UUID = Guid.NewGuid(),
+                CreateDate = DateTime.Now,
+                AlterDate = DateTime.Now,
+                Active = true,
+            };
+
+            Gender NoBinary = new()
+            {
+                Id = 3,
+                Name = "Não Binário",
+                IsBinary = false,
+                UUID = Guid.NewGuid(),
+                CreateDate = DateTime.Now,
+                AlterDate = DateTime.Now,
+                Active = true,
+            };
+
+            modelBuilder.Entity<Gender>().HasData(masc);
+            modelBuilder.Entity<Gender>().HasData(fem);
+            modelBuilder.Entity<Gender>().HasData(NoBinary);
         }
     }
 }
