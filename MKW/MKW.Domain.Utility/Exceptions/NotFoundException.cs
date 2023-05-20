743e485d2b3e21b59a76d7ff344f37b3b9eda797
pedroalcantara-net <pedrohla42@gmail.com>
2023-05-20 17:12:13 -0300

    feat: implementação de NotFoundException no Middleware de Requisições.

---
+++ MKW/MKW.Domain.Utility/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKW.Domain.Utility.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
