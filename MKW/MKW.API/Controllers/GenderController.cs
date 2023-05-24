01a2cdc89a8bc602d07e378050cf8f7edb8eac9f
pedroalcantara-net <pedrohla42@gmail.com>
2023-05-24 01:30:52 -0300

    feat: adicionado GET por gênero.

--- MKW/MKW.API/Controllers/GenderController.cs
+++ MKW/MKW.API/Controllers/GenderController.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MKW.API.Controllers.Base;
+using MKW.Domain.Dto.DTO.Base;
+using MKW.Domain.Dto.DTO.GenderDTO;
 using MKW.Domain.Entities.UserAggregate;
 using MKW.Domain.Interface.Services.AppServices;
 
 namespace MKW.API.Controllers
 {
     [Route("v1/[controller]")]
-    public class GenderController : CrudBaseController<Gender>
+    public class GenderController : BaseController
     {
 
         private readonly IGenderService _genderService;
 
-        public GenderController(IGenderService genderService) : base(genderService)
+        public GenderController(IGenderService genderService)
         {
             _genderService = genderService;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponseDTO<GenderDto>>> Get() => Ok(_genderService.Get());
     }
 }
--- MKW/MKW.Domain.Interface.Services/AppServices/IGenderService.cs
+++ MKW/MKW.Domain.Interface.Services/AppServices/IGenderService.cs
@@ -1,9 +1,12 @@
-﻿using MKW.Domain.Entities.UserAggregate;
+﻿using MKW.Domain.Dto.DTO.Base;
+using MKW.Domain.Dto.DTO.GenderDTO;
+using MKW.Domain.Entities.UserAggregate;
 using MKW.Domain.Interface.Services.AppServices.Base;
 
 namespace MKW.Domain.Interface.Services.AppServices
 {
-    public interface IGenderService : IBaseService<Gender>
+    public interface IGenderService
     {
+        Task<BaseResponseDTO<GenderDto>> Get();
     }
 }
