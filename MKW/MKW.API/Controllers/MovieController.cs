4337e22ebc78f28409a8894a94190acd0b0efde5
pedroalcantara-net <pedrohla42@gmail.com>
2023-05-24 11:29:56 -0300

    feat: documentação de métodos das rotas das APIs

--- MKW/MKW.API/Controllers/MovieController.cs
+++ MKW/MKW.API/Controllers/MovieController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MKW.API.Controllers.Base;
+using MKW.Domain.Dto.DTO.Base;
+using MKW.Domain.Dto.DTO.ReviewDTO;
 using MKW.Domain.Interface.Services.BaseServices;
+using System.Net.Mime;
 
 namespace MKW.API.Controllers
 {
@@ -16,6 +19,10 @@ namespace MKW.API.Controllers
         }
 
         [HttpGet("{movieId:int}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(BaseResponseDTO<object>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(BaseResponseDTO<object>))]
         public async Task<ActionResult<object>> GetMovie([FromRoute] int movieId) => Ok(await _tmdbService.GetMovie(movieId));
     }
 }
