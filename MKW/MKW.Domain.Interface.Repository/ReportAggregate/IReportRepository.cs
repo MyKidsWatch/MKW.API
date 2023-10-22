7b6f5cb100a1bd0c303699be58173b8735396a79
lukkanog <lukanog@hotmail.com>
2023-10-22 02:24:49 -0300

    fix: add reportedPersonId to the verification of already existing report

--- MKW/MKW.Domain.Interface.Repository/ReportAggregate/IReportRepository.cs
+++ MKW/MKW.Domain.Interface.Repository/ReportAggregate/IReportRepository.cs
@@ -5,6 +5,6 @@ namespace MKW.Domain.Interface.Repository.ReportAggregate
 {
     public interface IReportRepository : IBaseRepository<Report>
     {
-        Task<bool> AnyReportByUser(int personId, int? reviewId, int? commentId);
+        Task<bool> AnyReportByUser(int personId, int? reviewId, int? commentId, int? reportedPersonId);
     }
 }
