aaa6cff527a82aad8d989f4ad44b808adc132bbf
pedroalcantara-net <pedrohla42@gmail.com>
2023-11-23 12:28:16 -0300

    hotfix: validação do username de 12 caracteres no login

--- MKW/MKW.Domain.ViewModel/DTO/IdentityDTO/Authentication/LoginRequestByUserNameDTO.cs
+++ MKW/MKW.Domain.ViewModel/DTO/IdentityDTO/Authentication/LoginRequestByUserNameDTO.cs
@@ -5,7 +5,7 @@ namespace MKW.Domain.Dto.DTO.IdentityDTO.Authentication
     public class LoginRequestByUserNameDTO
     {
         [Required(ErrorMessage = "The field {0} is required")]
-        [MaxLength(12, ErrorMessage = "Max length 12")]
+        [MaxLength(20, ErrorMessage = "Max length 20")]
         [MinLength(3, ErrorMessage = "Min length 2")]
         public string UserName { get; set; }
 
